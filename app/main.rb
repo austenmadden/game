9989a0fc0a3734194836cbdf26326521b0ae5a03
Austen Madden <me@austenmadden.com>
2021-04-12 22:09:39 -0400

    Demonstrate Ruby concepts
    Starting a Dragonruby game to teach some Ruby basics as well as general
    programming to some friends.

--- app/main.rb
+++ app/main.rb
@@ -1,6 +1,11 @@
-def tick args
-  args.outputs.labels  << [640, 500, 'Hello World!', 5, 1]
-  args.outputs.labels  << [640, 460, 'Go to docs/docs.html and read it!', 5, 1]
-  args.outputs.labels  << [640, 420, 'Join the Discord! http://discord.dragonruby.org', 5, 1]
-  args.outputs.sprites << [576, 280, 128, 101, 'dragonruby.png']
+MID_Y = 350
+MID_X = 650
+SCALER = 3
+
+def tick(args)
+  label_args = [MID_X, MID_Y, "Hi"]
+  label_args << 20
+  label_args << 1
+  args.outputs.labels  << label_args
+  args.outputs.sprites << [576, 280, 128*SCALER, 101*SCALER, 'dragonruby.png']
 end
