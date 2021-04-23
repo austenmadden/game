44ffb8dd2eaa4e8b56b6d5fd5530615dce962e4d
Austen Madden <me@austenmadden.com>
2021-04-22 20:57:39 -0400

    Add movement to label

--- app/main.rb
+++ app/main.rb
@@ -3,9 +3,16 @@ MID_X = 650
 SCALER = 3
 
 def tick(args)
-  label_args = [MID_X, MID_Y, "Hi"]
+  args.state.x ||= 0
+  args.state.y ||= 0
+
+  args.state.y += args.inputs.up_down
+  args.state.x += args.inputs.left_right
+
+  label_args = [MID_X + args.state.x, MID_Y + args.state.y, "Hi"]
   label_args << 20
   label_args << 1
+
   args.outputs.labels  << label_args
   args.outputs.sprites << [576, 280, 128*SCALER, 101*SCALER, 'dragonruby.png']
 end
